123941b53468503b9b11d2f2f896bfbeb3cb9f14
Jason Wieringa <jason@jasonwieringa.com>
2014-10-15 22:09:26 -0400

    Add BigDesk and Paramedic Plugins

--- recipes/esplugins.rb
+++ recipes/esplugins.rb
@@ -18,4 +18,25 @@ script "install_plugin_es_hq" do
 	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/HQ") }
 end
 
+script "install_plugin_es_bigdesk" do
+	interpreter "bash"
+	user "root"
+	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+	code <<-EOH
+  	plugin -install lukas-vlcek/bigdesk
+  	EOH
+	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/bigdesk") }
+end
+
+script "install_plugin_es_paramedic" do
+	interpreter "bash"
+	user "root"
+	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+	code <<-EOH
+  	plugin -install karmi/elasticsearch-paramedic
+  	EOH
+	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/paramedic") }
+end
+
+
 #notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
